2907b5bd9886281dd776f43b368aade08c8e8805
Jerisson777 <jerisson.p@outlook.com>
2023-04-17 16:08:37 -0400

    first commit

---
+++ src/scenes/Testimonials.jsx
@@ -0,0 +1,93 @@
+import LineGradient from "../components/LineGradient";
+import { motion } from "framer-motion";
+
+const Testimonials = () => {
+  return (
+    <section id="testimonials" className="pt-32 pb-16">
+      {/* HEADING */}
+      <motion.div
+        className="md:w-1/3 text-center md:text-left"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+      >
+        <p className="font-playfair font-semibold text-4xl mb-5 text-red">
+          TESTIMONIALS
+        </p>
+        <LineGradient width="mx-auto w-2/5" />
+        <p className="mt-10">
+          Here's What People are Saying About My Work.
+        </p>
+      </motion.div>
+
+      {/* TESTIMONIALS */}
+      <div className="md:flex md:justify-between gap-8">
+        <motion.div
+          className="mx-auto relative bg-blue max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-50
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person1"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.6 }}
+          variants={{
+            hidden: { opacity: 0, scale: 0.8 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+        >
+          <p className="text-2xl font-bold p-5">Carlos Maxwell</p>
+          <p className="font-playfair text-6xl">“</p>
+          <p className="text-center text-xl">
+          upTask is an application with a great interface 
+          and interactivity that could be very useful in today's world.
+          </p>
+        </motion.div>
+
+        <motion.div
+          className="mx-auto relative bg-red max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-50
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person2"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.6 }}
+          variants={{
+            hidden: { opacity: 0, scale: 0.8 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+        >
+          <p className="text-2xl font-bold p-5">Edwin Isidro</p>
+          <p className="font-playfair text-6xl">“</p>
+          <p className="text-center text-xl">
+          The applications are so user-friendly that they even provide a 
+          complete guide on how to navigate through the app.
+          </p>
+        </motion.div>
+
+        <motion.div
+          className="mx-auto relative bg-yellow max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-50
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.6 }}
+          variants={{
+            hidden: { opacity: 0, scale: 0.8 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+        >
+          <p className="text-2xl font-bold p-10">Sarah Gonzales</p>
+          <p className="font-playfair text-6xl">“</p>
+          <p className="text-center text-xl">
+          The response time for each request to the backend is acceptable.
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
